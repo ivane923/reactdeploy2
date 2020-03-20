b5e4c53091669b652bfc777cf93786851fb43292
Ivane <csdserver101@gmail.com>
2020-03-20 02:44:38 -0500

    Updates

---
+++ precache-manifest.6fab8d13bfe863410c210ae70da72728.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99d484c65210403fa2744b169ea32c8",
+    "url": "/ivane923/reactdeploy2.git/index.html"
+  },
+  {
+    "revision": "f81656719f6333a530d8",
+    "url": "/ivane923/reactdeploy2.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9edc7e0f4a05000183a7",
+    "url": "/ivane923/reactdeploy2.git/static/js/2.661b7290.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ivane923/reactdeploy2.git/static/js/2.661b7290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f81656719f6333a530d8",
+    "url": "/ivane923/reactdeploy2.git/static/js/main.ad5f5e48.chunk.js"
+  },
+  {
+    "revision": "1a116afd59e2c097d4a6",
+    "url": "/ivane923/reactdeploy2.git/static/js/runtime-main.63c4ab16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ivane923/reactdeploy2.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
